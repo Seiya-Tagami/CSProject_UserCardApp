d184b87b2c4ce76cbb5464d97d3ebfd479289100
seiya_tagami <bepozurapu274@gmail.com>
2023-05-16 20:43:33 +0900

    feat: UI

--- Motivational_Speech_Wallpaper/main.js
+++ Motivational_Speech_Wallpaper/main.js
@@ -1,32 +1,51 @@
 'use strict';
 
-let domObj = document.getElementById('target');
+const domObj = document.getElementById('js-target');
+const genButton = document.getElementById('js-button');
 
 function motivationalSpeechWallpaper(text, colorCode, imgUrl, pos1, pos2) {
   const wallpaperDiv = document.createElement('div');
   const wallpaperImg = document.createElement('img');
+  const wallpaperP = document.createElement('p');
+
+  wallpaperDiv.classList.add('w-75', 'mx-auto', 'mt-4', 'position-relative');
+  wallpaperDiv.style.height = 'fit-content';
+
   wallpaperImg.src = imgUrl;
+  wallpaperImg.classList.add('w-100', 'h-auto');
+
+  wallpaperP.classList.add('h5', 'position-absolute', `${pos1}-50`, `start-50`, 'text-white', 'fs-3');
+  wallpaperP.innerText = text;
+
+  wallpaperDiv.append(wallpaperImg);
+  wallpaperDiv.append(wallpaperP);
+
+  domObj.append(wallpaperDiv);
 }
-motivationalSpeechWallpaper(
-  'Perfection is achieved, not when there is nothing more to add, but when there is nothing left to take away. - Antoine de Saint',
-  '1B4F72',
-  'https://cdn.pixabay.com/photo/2020/06/12/03/06/magnifying-glass-5288877__340.jpg',
-  'top',
-  'right'
-);
 
-motivationalSpeechWallpaper(
-  'The scientist discovers a new type of material or energy and the engineer discovers a new use for it. - Gordon Lindsay Glegg',
-  '007bff',
-  'https://cdn.pixabay.com/photo/2018/02/23/04/38/laptop-3174729_1280.jpg',
-  'center',
-  'left'
-);
+genButton.addEventListener('click', () => {
+  domObj.innerHTML = '';
+  motivationalSpeechWallpaper(
+    'Perfection is achieved, not when there is nothing more to add, but when there is nothing left to take away. - Antoine de Saint',
+    '1B4F72',
+    'https://cdn.pixabay.com/photo/2020/06/12/03/06/magnifying-glass-5288877__340.jpg',
+    'top',
+    'right'
+  );
+});
 
-motivationalSpeechWallpaper(
-  'Scientists study the world as it is, engineers create the world that never has been. - Theodore von Karman',
-  'ecf0f1',
-  'https://cdn.pixabay.com/photo/2017/05/10/19/29/robot-2301646_1280.jpg',
-  'center',
-  'center'
-);
+// motivationalSpeechWallpaper(
+//   'The scientist discovers a new type of material or energy and the engineer discovers a new use for it. - Gordon Lindsay Glegg',
+//   '007bff',
+//   'https://cdn.pixabay.com/photo/2018/02/23/04/38/laptop-3174729_1280.jpg',
+//   'center',
+//   'left'
+// );
+
+// motivationalSpeechWallpaper(
+//   'Scientists study the world as it is, engineers create the world that never has been. - Theodore von Karman',
+//   'ecf0f1',
+//   'https://cdn.pixabay.com/photo/2017/05/10/19/29/robot-2301646_1280.jpg',
+//   'center',
+//   'center'
+// );
